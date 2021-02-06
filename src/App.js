b3a405b5ba00108eb25e5a3d65bbe203f7eb007f
Vincent Lafrance <vincent.lafrance.2@ulaval.ca>
2021-02-05 21:04:25 -0500

    v4

--- src/App.js
+++ src/App.js
@@ -3,24 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>
-          Hello World!
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Yo this is a great hack, could I get participation points?
+      </p>
     </div>
   );
 }
